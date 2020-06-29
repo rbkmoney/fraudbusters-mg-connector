9d36059549197d0905c2c25e30695cdbbfd2db2a
k.struzhkin <k.struzhkin@MacBook-Pro-rbkadmin.local>
2020-06-29 13:12:21 +0300

    Init commit

---
+++ src/main/java/com/rbkmoney/fraudbustersmgconnector/FraudbustersMgConnectorApplication.java
@@ -0,0 +1,15 @@
+package com.rbkmoney.fraudbustersmgconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class FraudbustersMgConnectorApplication extends SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudbustersMgConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rbkmoney/fraudbustersmgconnector/FraudbustersMgConnectorApplicationTest.java
@@ -0,0 +1,16 @@
+package com.rbkmoney.fraudbustersmgconnector;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = FraudbustersMgConnectorApplication.class)
+public class FraudbustersMgConnectorApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
